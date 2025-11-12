f2bffc427caa7c07ef7abe1e3a0e009028d6052d
Marsela0603 <marselasela3554@gmail.com>
2025-11-12 10:06:19 +0700

    authentication implemented

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -3,6 +3,8 @@ const { nanoid } = require('nanoid'); // untuk generate id unik
 const bcrypt = require('bcrypt'); // untuk hash password
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
+
 
 class UsersService {
   constructor() {
@@ -53,6 +55,27 @@ async verifyNewUsername(username) {
     }
  
     return result.rows[0];
+  }
+
+   async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+ 
+    const result = await this._pool.query(query);
+ 
+    if (!result.rows.length) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+ 
+    const { id, password: hashedPassword } = result.rows[0];
+    const match = await bcrypt.compare(password, hashedPassword);
+
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+    return id;
   }
 }
 
